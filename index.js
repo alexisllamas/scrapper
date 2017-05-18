a72dbde003d70fce57b2e33d697364b56f080bd6
alexisllamas <alexisnavarro23@gmail.com>
2017-05-18 16:58:45 +0000

    scrapper ftw!

---
+++ index.js
@@ -0,0 +1,25 @@
+const request = require('superagent');
+const cheerio = require('cheerio');
+
+const scrapp = (url) => {
+  request
+    .get(url)
+    .end((err, res) => {
+      const $ = cheerio.load(res.text);
+      const links = $('a').toArray();
+      console.log(links.length);
+      links.filter(el => el.attribs.href && el.attribs.href.startsWith(url)).forEach((el) => {
+        request
+          .get(el.attribs.href)
+          .end((err, res) => {
+            const $ = cheerio.load(res.text);
+            const products = $('li.grid-tile').filter('.product-tile').toArray();
+            products.forEach((li) => {
+              console.log(li)
+            })
+          });
+      })
+    });
+};
+
+scrapp('http://www.americangolf.co.uk/')
